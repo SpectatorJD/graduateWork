f90686c14b323f5e00327f4f3d363352f214a49f
Deanisgarayev <garaevdenis2019@gmail.com>
2023-12-19 23:02:35 +0500

    fixed dto

--- src/main/java/ru/skypro/homework/dto/Ads.java
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -1,9 +1,14 @@
 package ru.skypro.homework.dto;
 
+import antlr.collections.List;
 import lombok.Data;
 
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 @Data
 public class Ads {
@@ -12,7 +17,7 @@ public class Ads {
     //    count	integer($int32)
 //    общее количество объявлений
 
-    private Ad[] results;
+    private Collection<Ad> results;
 //    results	[Ad{Jump to definition
 //        author	integer($int32)
 //                id автора объявления
